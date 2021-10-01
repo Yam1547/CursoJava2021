b8be21f97f445ab3e22756bbe150f24c84cce6d8
Yamila_Reyes <55166800+Yam1547@users.noreply.github.com>
2021-10-01 11:53:47 -0300

    MOD

--- CursoJava/src/Ejercicio1.java
+++ CursoJava/src/Ejercicio1.java
@@ -4,6 +4,15 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println( "Nombre \t apellido \t email \t teléfono\n------ \t ------ \t ------ \t ------\n Xxxxx \t xxxxxx \t xxxxxx \t xxxxxx");
+		
+		
+		System.out.println( "Nombre\t apellido\t email \t teléfono\n------ \t ------ \t ------ \t ------\n Xxxxx \t xxxxxx \t xxxxxx \t xxxxxx");
+		
+
+		
+	
+	
 	}
 
 }
