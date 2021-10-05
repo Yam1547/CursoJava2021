9cb055ac5d6b4613c2dd1b3c59cd405b67c40679
Yamila_Reyes <55166800+Yam1547@users.noreply.github.com>
2021-10-05 01:25:28 -0300

    mod

--- CursoJava/src/modulo1/Ejercicio1.java
+++ CursoJava/src/modulo1/Ejercicio1.java
@@ -8,9 +8,19 @@ public class Ejercicio1 {
 		
 
 		//Práctica
-	   System.out.println( "Nombre\tapellido\temail\tteléfono\n------ \t ------ \t ------\t--------\n Xxxxx \t xxxxxx \t xxxxxx xxxxxx");
+	   //System.out.println( "Nombre\tapellido\temail\tteléfono\n------ \t ------ \t ------\t--------\n Xxxxx \t xxxxxx \t xxxxxx xxxxxx");
 
-	  
+	  System.out.println("Nombre\tApellido\tEmail\tTelefono");
+	  System.out.println("\n------\t------\t------\t------");
+	  System.out.println("\nXxxxxx\tXxxxxx\tXxxxxx\tXxxxxx");
+	
+		
+	  System.out.println("\nHola Renata");
+	  System.out.println("\n\n\nnombre\tapellido\temail");
+	  System.out.println("\nGabriel\tCasas\tgcasas@gmail.com");
+	  System.out.println("\"El Jedi\"");
+	  System.out.println("la barra \\");
+	   
 
 			
 	}
---
+++ CursoJava/src/modulo1/Ejercicio3.java
@@ -0,0 +1,19 @@
+package modulo1;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		  System.out.println("Tecla de escape\t\tSignificado\n");
+		  System.out.println("\\n\t\t\tSignifica nueva línea");
+		  System.out.println("\"\"\t\t\tEs para poner comillas dobles dentro del texto");
+		  System.out.println("\\\\\t\t\tSe utiliza para escribir la \\ dentro del texto");
+		  System.out.println("\\'\t\t\tSe utiliza para las comillas simples dentro del texto");
+		 
+		
+		   
+		
+	}
+
+}
