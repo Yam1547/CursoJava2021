20715b57b9b49a9a02fc41122765d7a1ebc3994c
Yamila_Reyes <55166800+Yam1547@users.noreply.github.com>
2021-10-05 21:55:36 -0300

    mod icons

--- CursoJava/src/modulo2/Ejercicio4.java
+++ CursoJava/src/modulo2/Ejercicio4.java
@@ -5,10 +5,16 @@ public class Ejercicio4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		byte b=10;
+		byte b= (byte) (Math.random()*Byte.MAX_VALUE);
 		short s=20;
 		int i =30;
 		long l=40;
+		
+		int sumabb= b+b;
+		int sumabs= b+s;
+		int sumabi= b+i;
+		int sumaii= i+i;
+		long sumasl= s+l;
 		
 		System.out.println(b=(byte) s);
 		//System.out.println(s);
--- CursoJava/src/modulo3/Pantalla1.java
+++ CursoJava/src/modulo3/Pantalla1.java
@@ -11,6 +11,8 @@ import java.awt.Font;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
 public class Pantalla1 {
 
@@ -93,6 +95,12 @@ public class Pantalla1 {
 		textField_2.setBounds(63, 163, 86, 20);
 		frame.getContentPane().add(textField_2);
 		
+		
+		JLabel lblNewLabel_3 = new JLabel("");	
+		lblNewLabel_3.setBounds(351, 98, 56, 56);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		
 		JButton btnNewButton = new JButton("Calcular ");
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 12));
 		btnNewButton.addActionListener(new ActionListener() {
@@ -103,29 +111,36 @@ public class Pantalla1 {
 				
 				float promedio = (nota1 +nota2 +nota3)/3;
 				lblNewLabel_2_1.setText(Float.toString(promedio));
+							
 				
 				if(promedio >=7) {
-					lblNewLabel_2_1.setForeground(Color.blue);
+					lblNewLabel_2_1.setForeground(Color.blue);				
+					lblNewLabel_3.setIcon(new ImageIcon("C:\\Users\\yjr15\\Downloads\\icons8-koya-bt21-48.png"));
+					
 				} else {
 					lblNewLabel_2_1.setForeground(Color.red);
+					lblNewLabel_3.setIcon(new ImageIcon("C:\\Users\\yjr15\\Downloads\\icons8-scream-48.png"));
 				}
 			}
 		});
 		
-		btnNewButton.setBounds(192, 213, 114, 37);
+		btnNewButton.setBounds(179, 213, 114, 37);
 		frame.getContentPane().add(btnNewButton);
+		
 		
 		lblNewLabel_2 = new JLabel("Promedio");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.ITALIC, 14));
-		lblNewLabel_2.setBounds(267, 126, 72, 14);
+		lblNewLabel_2.setBounds(259, 88, 72, 14);
 		frame.getContentPane().add(lblNewLabel_2);
 		
 		lblNewLabel_2_1 = new JLabel("Resultado");
 		lblNewLabel_2_1.setForeground(Color.BLACK);
-		lblNewLabel_2_1.setBackground(Color.YELLOW);
+		lblNewLabel_2_1.setBackground(Color.WHITE);
 		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblNewLabel_2_1.setBounds(267, 138, 107, 41);
+		lblNewLabel_2_1.setBounds(255, 113, 86, 41);
 		frame.getContentPane().add(lblNewLabel_2_1);
+		
+		
 		
 	}
 }
