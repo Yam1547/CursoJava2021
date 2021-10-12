b5be7838aa146820dd77201c74858bb75e7380a0
Yamila_Reyes <55166800+Yam1547@users.noreply.github.com>
2021-10-12 16:16:31 -0300

    Create Ejercicio2.java

---
+++ CursoJava/src/modulo5/Ejercicio2.java
@@ -0,0 +1,22 @@
+package modulo5;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+			for (int i =0; i <20; i++) {
+				//si i = 2 resto vale 0 no se suma
+				// acá solamente se van a sumar números impares
+				int resto = i %2;
+			if(i%2 == 1)
+				sumo +=i;
+			}
+			
+			
+			sumo = sumo + i * i%2 //--> Algoritmo
+	}
+
+}
