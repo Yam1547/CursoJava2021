876e3ef48d64102cc1ac6d6a4a18efc64d2397be
Yamila_Reyes <55166800+Yam1547@users.noreply.github.com>
2021-10-12 10:24:24 -0300

    Modif.  uso del For para recorrer string

--- CursoJava/src/modulo5/Ejercicio1.java
+++ CursoJava/src/modulo5/Ejercicio1.java
@@ -1,9 +1,50 @@
 package modulo5;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Ingrese un texto a codificar en clave murciélago");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//MURCIELAGO
+		//0123456789
+		
+		String strTextoOriginal = sc.nextLine();
+		
+		System.out.println("Texto original=" + strTextoOriginal);
+		System.out.println("Todo en mayúscula=" + strTextoOriginal.toUpperCase());
+		System.out.println("Todo en minúscula=" + strTextoOriginal.toLowerCase());
+		System.out.println("Texto original=" + strTextoOriginal);
+		
+		System.out.println("\n\n\ny la clave murciélago para cuándo?, ahora");
+		
+		//Llamado on the fly, ...llamar a objetos
+		
+		String strMurcielago = strTextoOriginal.toLowerCase()
+			                                   .replace('m', '0')
+			                                   .replace('u', '1')
+			                                   .replace('r', '2')
+			                                   .replace('c', '3')
+			                                   .replace('i', '4')
+			                                   .replace('e', '5')
+			                                   .replace('l', '6')
+			                                   .replace('a', '7')
+			                                   .replace('g', '8')
+			                                   .replace('o', '9')
+											   .toUpperCase();
+		
+		System.out.println("En clave murciélago=" + strMurcielago);
+		
+		
+		//Recorro el string
+		
+		for(int i =0; i < strTextoOriginal.length(); i ++)
+			System.out.println("posición= " + i  + " char= " + strTextoOriginal.charAt(i));
 
 	}
 
