a34f4c13e849c181f72085b8740cbe77a927b3b5
Yamila_Reyes <55166800+Yam1547@users.noreply.github.com>
2021-10-14 11:00:40 -0300

    Testeo, uso del JUnit

--- CursoJava/src/util/DateUtil.java
+++ CursoJava/src/util/DateUtil.java
@@ -16,14 +16,49 @@ public class DateUtil {
 	//Este método devuelve el año de la fecha que se envía por parámetro
 	//@param pFecha corresponde al parámetro donde se envía la fecha
 	//Return devuelve valor tipo int
-	public static int getAnio(Date pFecha ) {;
-	Calendar cal = Calendar.getInstance();
-	cal.setTime(pFecha);
+	public static int getAnio(Date fecha ) {
+		
+	Calendar cal = Calendar.getInstance();//factory, singleton
+	
+	cal.setTime(fecha);
 	
 	return cal.get(Calendar.YEAR);
 }
-
-
+	
+	public static int getMes(Date fecha ) {
+		
+	Calendar cal = Calendar.getInstance();//factory, singleton
+	
+	cal.setTime(fecha);
+	
+	return cal.get(Calendar.MONTH)+1;
+}
+	
+	public static int getDia(Date fecha ) {
+		
+	Calendar cal = Calendar.getInstance();//factory, singleton
+	
+	cal.setTime(fecha);
+	
+	return cal.get(Calendar.DATE);
+}
+	
+//	public static boolean isFinDeSemana(Date fecha) {
+//
+//	Calendar cal = Calendar.getInstance();// factory, singleton
+//
+//	cal.setTime(fecha);
+//
+//	if (fecha == "Sábado" && "Domingo")
+//
+//		return cal.get(Calendar.DAY_OF_WEEK, "");
+//}
+	
+	
+	
+	
+	
+	
 
 
 }
--- CursoJava/src/util/test/DateUtilTest.java
+++ CursoJava/src/util/test/DateUtilTest.java
@@ -1,41 +1,63 @@
 package util.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 import java.util.Calendar;
 import java.util.Date;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import util.DateUtil;
 
-class DateUtilTest {
+public class DateUtilTest {
+	
 	//lote de pruebas
 	Date fechaCumple;
+		
+	//Junit --> Framework, conjunto de clases, resuelve problemas particulares. Por ej: el testeo
+	//Patrón de diseño --> General, para resolver problemas
 	
 	//Antes de cada prueba
 	@Before
-	public void setUp() throws Exception{
+	public void setUp() throws Exception {
 		Calendar cal = Calendar.getInstance();
 		cal.set(1989, Calendar.JUNE, 15);
 		fechaCumple = cal.getTime();
-		
 	}
 	
 	//Después de cada  prueba
 	@After
-	public void tearDown() throws Exception{
+	public  void tearDown() throws Exception {
 		fechaCumple = null;
-		
+	}
+	
+//	@Test
+//	public void testGetAnio1989() {
+//		fechaCumple.setYear(1989);
+//		assertEquals(1989, DateUtil.getAnio(fechaCumple));
+//		
+//	}
+	
+	@Test
+	public void testGetAnio() {
+		assertEquals(1989, DateUtil.getAnio(fechaCumple));
+	}
+	
+	@Test
+	public void testGetMes() {
+		assertEquals(6, DateUtil.getMes(fechaCumple));
 	}
 	
 
 	@Test
-	void testGetAnio() {
-		assertEquals(1989, DateUtil.getAnio(fechaCumple));
-		
+	public void testGetDia() {
+		assertEquals(15, DateUtil.getDia(fechaCumple));
 	}
+
+
 
 }
